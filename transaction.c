1fadc68fcc188143ad18248302bf00d2b8898a4f
Xavier Yao <xavieryao@live.cn>
2018-04-03 19:45:24 +0800

    add sig

--- transaction.c
+++ transaction.c
@@ -17,6 +17,7 @@ static transaction_queue_t queue;
 static void append_front(transaction_node_t* node);
 static void append_back(transaction_node_t* node);
 static void remove_from_queue(transaction_node_t* node);
+void finish_transaction(int efd, transaction_t* trans);
 
 void init_transaction(transaction_t* trans) {
     trans->fd = INVALID_FD;
